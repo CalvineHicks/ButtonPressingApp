66c6bda653cbca6630f56c844df9ec7907a37ec3
Calviepooh <calviepooh@Calviepoohs-MacBook-Pro.local>
2018-06-03 13:37:08 -0600

    updates to button config

--- app/src/main/java/com/medical/anschutz/cu/buttonpressingapp/model/ButtonConfig.java
+++ app/src/main/java/com/medical/anschutz/cu/buttonpressingapp/model/ButtonConfig.java
@@ -1,75 +1,100 @@
 package com.medical.anschutz.cu.buttonpressingapp.model;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
+
+import com.medical.anschutz.cu.buttonpressingapp.services.dataTransformers;
 
 import java.io.Serializable;
 
 public class ButtonConfig implements Serializable{
 
+    dataTransformers dataTransformers = new dataTransformers();
+
+    private String text = Defaults.DEFAULT_BUTTON_TEXT;
+    private String font = Defaults.DEFAULT_BUTTON_TEXT_FONT;
+    private int style = Defaults.DEFAULT_BUTTON_TEXT_STYLE;
+    private String gravity = Defaults.DEFAULT_BUTTON_TEXT_GRAVITY;
+    private int backgroundColor = Defaults.DEFAULT_BUTTON_COLOR;
     private Integer height = Defaults.DEFAULT_BUTTON_HEIGHT;
     private Integer width = Defaults.DEFAULT_BUTTON_WIDTH;
+    private Integer rotation = Defaults.DEFAULT_BUTTON_ROTATION;
     private Integer xPosition = Defaults.DEFAULT_BUTTON_X_POSITION;
     private Integer yPosition = Defaults.DEFAULT_BUTTON_Y_POSITION;
-    private Integer rotation = Defaults.DEFAULT_BUTTON_ROTATION;
 
     //Text and Styling
-    private String text = Defaults.DEFAULT_BUTTON_TEXT;
-    private int backgroundColor = Defaults.DEFAULT_BUTTON_COLOR;
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-
-    public Integer getWidth() {
-        return width;
-    }
-
-    public void setWidth(Integer width) {
-        this.width = width;
-    }
-
-    public Integer getxPosition() {
-        return xPosition;
-    }
-
-    public void setxPosition(Integer xPosition) {
-        this.xPosition = xPosition;
-    }
-
-    public Integer getyPosition() {
-        return yPosition;
-    }
-
-    public void setyPosition(Integer yPosition) {
-        this.yPosition = yPosition;
-    }
-
-    public Integer getRotation() {
-        return rotation;
-    }
-
-    public void setRotation(Integer rotation) {
-        this.rotation = rotation;
-    }
-
     public String getText() {
         return text;
     }
-
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getFont() {
+        return font;
+    }
+    public void setFont(String font) {
+        this.font = font;
+    }
+
+    public int getStyle() {
+        return style;
+    } //bold, italic, normal
+    public void setStyle(int style) {
+        this.style = style;
+    }
+
+    public String getGravity() {
+        return gravity;
+    }
+    public void setGravity(String gravity) {
+        this.gravity = gravity;
     }
 
     public int getBackgroundColor() {
         return backgroundColor;
     }
-
     public void setBackgroundColor(int backgroundColor) {
         this.backgroundColor = backgroundColor;
+    }
+
+
+    //Button Dimensions and Orientation
+    public Integer getHeight() {
+        return height;
+    }
+    public void setHeight(Integer height) {
+        this.height = dataTransformers.convertDpToPx(height);
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+    public void setWidth(Integer width) {
+        this.width = dataTransformers.convertDpToPx(width);
+    }
+
+
+    public Integer getRotation() {
+        return rotation;
+    }
+    public void setRotation(Integer rotation) {
+        this.rotation = rotation;
+    }
+
+    //Button Location
+    public Integer getxPosition() {
+        return xPosition;
+    }
+    public void setxPosition(Integer xPosition) {
+        this.xPosition = dataTransformers.convertDpToPx(xPosition);
+    }
+
+    public Integer getyPosition() {
+        return yPosition;
+    }
+    public void setyPosition(Integer yPosition) {
+        this.yPosition = dataTransformers.convertDpToPx(yPosition);
     }
 
 }
--- app/src/main/java/com/medical/anschutz/cu/buttonpressingapp/model/Defaults.java
+++ app/src/main/java/com/medical/anschutz/cu/buttonpressingapp/model/Defaults.java
@@ -1,6 +1,10 @@
 package com.medical.anschutz.cu.buttonpressingapp.model;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Defaults {
     /* Screen */
@@ -23,5 +27,9 @@ public class Defaults {
     //text settings
     public static final String DEFAULT_BUTTON_TEXT = "TEXT";
     public static final int DEFAULT_BUTTON_COLOR = Color.GRAY;
-
+    public static final String DEFAULT_BUTTON_TEXT_FONT = "roboto";
+    public static final int DEFAULT_BUTTON_TEXT_STYLE = Typeface.NORMAL;
+    public static final String DEFAULT_BUTTON_TEXT_GRAVITY = "center_horizontal | center_vertical";
+    public static final ArrayList DEFAULT_BUTTON_TEXT_PADDING = new ArrayList<Integer>(Arrays.asList(0, 0,0,0));
+    public static final ArrayList<Integer> DEFAULT_BUTTON_MARGINS = new ArrayList<Integer>(Arrays.asList(0, 0,0,0));
 }
